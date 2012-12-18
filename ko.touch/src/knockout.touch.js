9d666da1399c4a4c1271f6a6fba2b5aff6b23c8b
Mainmatsu <Mainmatsu@gmail.com>
2012-12-18 13:10:24 +0800

    add some handlers and tests

--- ko.touch/src/knockout.touch.js
+++ ko.touch/src/knockout.touch.js
@@ -1,5 +1,5 @@
 ﻿/// <reference path="lib/knockout-2.2.0.debug.js" />
-(function(ko, undefined){
+(function (ko, undefined) {
     if (typeof (ko) === undefined) { throw "knockout is required for knockout.touch" }
 
     ko.bindingHandlers['invisible'] = {
@@ -14,13 +14,13 @@
 
     ko.bindingHandlers.tap = {
         init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            var handler = ko.utils.unwrapObservable(valueAccessor());            
+            var handler = ko.utils.unwrapObservable(valueAccessor());
             $(element).hammer({
-                    prevent_default: false,
-                    drag_vertical: false
+                prevent_default: false,
+                transform_vertical: false
             })
             .bind("tap", function (ev) {
-                handler(ev);                
+                handler(ev);
             });
         },
         update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
@@ -29,5 +29,109 @@
             //Later I can write my own implementation. I checked hammer, not that complicated.
         }
     }
+
+    ko.bindingHandlers.swipe = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var handler = ko.utils.unwrapObservable(valueAccessor());
+            $(element).hammer({
+                prevent_default: false,
+                transform_vertical: false
+            })
+            .bind("swipe", function (ev) {
+                handler(ev);
+            });
+        },
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            //something like react on tap and provide a tap handler, with tap event arguements.
+            //currently I can use something like hammer to track the gestures. 
+            //Later I can write my own implementation. I checked hammer, not that complicated.
+        }
+    }
+
+    ko.bindingHandlers.drag = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var handler = ko.utils.unwrapObservable(valueAccessor());
+            $(element).hammer({
+                prevent_default: true
+            })
+            .bind("drag", function (ev) {
+                handler(ev);
+            });
+        },
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            //something like react on tap and provide a tap handler, with tap event arguements.
+            //currently I can use something like hammer to track the gestures. 
+            //Later I can write my own implementation. I checked hammer, not that complicated.
+        }
+    }
+
+    ko.bindingHandlers.transform = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var handler = ko.utils.unwrapObservable(valueAccessor());
+            $(element).hammer({
+                prevent_default: true
+            })
+            .bind("transform", function (ev) {
+                handler(ev);
+            });
+        },
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            //something like react on tap and provide a tap handler, with tap event arguements.
+            //currently I can use something like hammer to track the gestures. 
+            //Later I can write my own implementation. I checked hammer, not that complicated.
+        }
+    }
+
+    ko.bindingHandlers.hold = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var handler = ko.utils.unwrapObservable(valueAccessor());
+            $(element).hammer({
+                prevent_default: true
+            })
+            .bind("hold", function (ev) {
+                handler(ev);
+            });
+        },
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            //something like react on tap and provide a tap handler, with tap event arguements.
+            //currently I can use something like hammer to track the gestures. 
+            //Later I can write my own implementation. I checked hammer, not that complicated.
+        }
+    }
+
+    ko.bindingHandlers.doubletap = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var handler = ko.utils.unwrapObservable(valueAccessor());
+            $(element).hammer({
+                prevent_default: true
+            })
+            .bind("doubletap", function (ev) {
+                handler(ev);
+            });
+        },
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            //something like react on tap and provide a tap handler, with tap event arguements.
+            //currently I can use something like hammer to track the gestures. 
+            //Later I can write my own implementation. I checked hammer, not that complicated.
+        }
+    }
+
+    ko.bindingHandlers.release = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var handler = ko.utils.unwrapObservable(valueAccessor());
+            $(element).hammer({
+                prevent_default: true
+            })
+            .bind("release", function (ev) {
+                handler(ev);
+            });
+        },
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            //something like react on tap and provide a tap handler, with tap event arguements.
+            //currently I can use something like hammer to track the gestures. 
+            //Later I can write my own implementation. I checked hammer, not that complicated.
+        }
+    }
+
 
 }(window.ko))
--- ko.touch/tests/tap.js
+++ ko.touch/tests/tap.js
@@ -1,20 +1,125 @@
 ﻿test("tap binding test", function () {
     var div = document.getElementById("tapDiv");
-    //TODO: make tap event implementation.
     var model = {
         wasCalled: false,
         doCall: function (arg1) {
             equal(model.wasCalled, false, "Is false before tap");
             model.wasCalled = true;
             equal(model.wasCalled, true, "Is true after tap");
-            //equal(arg1, model, "First arg is a model");
-            //equal(arg2.type, "click", "Second arg is event");
         }
     };
     div.innerHTML = "<button data-bind='tap:doCall'>tap</button>";
     ko.applyBindings(model, div);    
     
     ko.utils.triggerEvent(div.childNodes[0], "tap");
+    console.log("wascalled: " + model.wasCalled);
+    equal(model.wasCalled, true, "We expect tap was called");
+});
+
+test("swipe binding test", function () {
+    var div = document.getElementById("swipeDiv");
+    var model = {
+        wasCalled: false,
+        doCall: function (arg1) {
+            equal(model.wasCalled, false, "Is false before swipe");
+            model.wasCalled = true;
+            equal(model.wasCalled, true, "Is true after swipe");
+        }
+    };
+    div.innerHTML = "<button data-bind='swipe:doCall'>tap</button>";
+    ko.applyBindings(model, div);
+
+    ko.utils.triggerEvent(div.childNodes[0], "swipe");
+    console.log("wascalled: " + model.wasCalled);
+    equal(model.wasCalled, true, "We expect tap was called");
+});
+
+test("drag binding test", function () {
+    var div = document.getElementById("dragDiv");
+    var model = {
+        wasCalled: false,
+        doCall: function (arg1) {
+            equal(model.wasCalled, false, "Is false before drag");
+            model.wasCalled = true;
+            equal(model.wasCalled, true, "Is true after drag");
+        }
+    };
+    div.innerHTML = "<button data-bind='drag:doCall'>tap</button>";
+    ko.applyBindings(model, div);
+
+    ko.utils.triggerEvent(div.childNodes[0], "drag");
+    console.log("wascalled: " + model.wasCalled);
+    equal(model.wasCalled, true, "We expect tap was called");
+});
+
+test("transform binding test", function () {
+    var div = document.getElementById("transformDiv");
+    var model = {
+        wasCalled: false,
+        doCall: function (arg1) {
+            equal(model.wasCalled, false, "Is false before transform");
+            model.wasCalled = true;
+            equal(model.wasCalled, true, "Is true after transform");
+        }
+    };
+    div.innerHTML = "<button data-bind='transform:doCall'>tap</button>";
+    ko.applyBindings(model, div);
+
+    ko.utils.triggerEvent(div.childNodes[0], "transform");
+    console.log("wascalled: " + model.wasCalled);
+    equal(model.wasCalled, true, "We expect tap was called");
+});
+
+test("hold binding test", function () {
+    var div = document.getElementById("holdDiv");
+    var model = {
+        wasCalled: false,
+        doCall: function (arg1) {
+            equal(model.wasCalled, false, "Is false before hold");
+            model.wasCalled = true;
+            equal(model.wasCalled, true, "Is true after hold");
+        }
+    };
+    div.innerHTML = "<button data-bind='hold:doCall'>tap</button>";
+    ko.applyBindings(model, div);
+
+    ko.utils.triggerEvent(div.childNodes[0], "hold");
+    console.log("wascalled: " + model.wasCalled);
+    equal(model.wasCalled, true, "We expect tap was called");
+});
+
+test("doubletap binding test", function () {
+    var div = document.getElementById("doubletapDiv");
+    var model = {
+        wasCalled: false,
+        doCall: function (arg1) {
+            equal(model.wasCalled, false, "Is false before doubletap");
+            model.wasCalled = true;
+            equal(model.wasCalled, true, "Is true after doubletap");
+        }
+    };
+    div.innerHTML = "<button data-bind='doubletap:doCall'>tap</button>";
+    ko.applyBindings(model, div);
+
+    ko.utils.triggerEvent(div.childNodes[0], "doubletap");
+    console.log("wascalled: " + model.wasCalled);
+    equal(model.wasCalled, true, "We expect tap was called");
+});
+
+test("release binding test", function () {
+    var div = document.getElementById("releaseDiv");
+    var model = {
+        wasCalled: false,
+        doCall: function (arg1) {
+            equal(model.wasCalled, false, "Is false before release");
+            model.wasCalled = true;
+            equal(model.wasCalled, true, "Is true after release");
+        }
+    };
+    div.innerHTML = "<button data-bind='release:doCall'>tap</button>";
+    ko.applyBindings(model, div);
+
+    ko.utils.triggerEvent(div.childNodes[0], "release");
     console.log("wascalled: " + model.wasCalled);
     equal(model.wasCalled, true, "We expect tap was called");
 });
