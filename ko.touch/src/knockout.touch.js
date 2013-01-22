68c0abc697cb6182235ec4ac27096b6669ee1143
Mainmatsu <mainmatsu@gmail.com>
2013-01-23 06:03:14 +0800

    many changes

--- ko.touch/src/knockout.touch.js
+++ ko.touch/src/knockout.touch.js
@@ -1,9 +1,93 @@
 ﻿/// <reference path="lib/knockout-2.2.0.debug.js" />
-(function (ko, undefined) {    
+!function (ko, undefined) {
     if (typeof (ko) === "undefined") {
         throw "knockout is required for knockout.touch";
     }
-    
+    /*this равна undefined после использования 'use strict'; 
+    *можно использовать standard self-executing anonymous function
+    *но я не уверен что это хороший подход 
+    */
+    var _this = this;
+    'use strict';
+
+    var _has_touch = 'ontouchstart' in document,
+        interceptEvent,
+        handler, eventList,
+        gestures, i;
+
+
+    function isFunction(obj) {
+        return Object.prototype.toString.call(obj) == '[object Function]';
+    }
+
+    function addEvent(obj, type, fn) {
+        if (obj.attachEvent) {
+            obj['e' + type + fn] = fn;
+            obj[type + fn] = function () { obj['e' + type + fn](window.event); }
+            obj.attachEvent('on' + type, obj[type + fn]);
+        } else
+            obj.addEventListener(type, fn, false);
+    }
+
+    function triggerTouchEvent(element, eventType, event) {
+        if (isFunction(element['on' + eventType])) {
+            element['on' + eventType](event);
+        }
+    }
+
+    gestures = {
+        tap: function tap(event) {
+            //need for test
+            event.testField = 'TestValue';
+            //IE fix
+            var target = event.target || event.srcElement;
+            triggerTouchEvent(target, 'tap', event);
+        }
+    }
+
+    function handleEvents(event) {
+        //IE fix
+        event = event || window.event;
+        switch (event.type) {
+            case 'mousedown':
+            case 'touchstart':
+                gestures.tap(event);
+                break;
+
+            case 'mousemove':
+            case 'touchmove':
+
+                break;
+
+            case 'mouseup':
+            case 'mouseout':
+            case 'touchcancel':
+            case 'touchend':
+
+                break;
+        }
+    }
+
+    //needs renaming interceptEvent,touchOptions
+    interceptEvent = {
+        bindHandler: function (elem, eventType, handler) {
+            elem['on' + eventType] = handler;
+
+            if (_has_touch) {
+                eventList = ['touchstart', 'touchmove', 'touchend', 'touchcancel'];
+            }
+                // for non-touch
+            else {
+                eventList = ['mouseup', 'mousedown', 'mousemove', 'mouseout'];
+            }
+            for (i = 0; i < eventList.length; i++) {
+                addEvent(elem, eventList[i], handleEvents);
+            }
+
+
+        }
+    }
+
     ko.bindingHandlers['invisible'] = {
         update: function (element, valueAccessor) {
             var newValueAccessor = function () {
@@ -15,60 +99,37 @@
     };
 
     ko.bindingHandlers.tap = {
-        init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            var handler = ko.utils.unwrapObservable(valueAccessor());
-            
-            $(element).hammer({
-                prevent_default: false,
-                transform_vertical: false
-            })
-            .bind("tap", function (ev) {
-                    handler(ev);
-            });
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            handler = ko.utils.unwrapObservable(valueAccessor());
+            interceptEvent.bindHandler(element, "tap", handler);
         },
-        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {            
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
         }
     };
 
     ko.bindingHandlers.swipe = {
-        init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            var handler = ko.utils.unwrapObservable(valueAccessor());
-            $(element).hammer({
-                prevent_default: false,
-                transform_vertical: false
-            })
-                .bind("swipe", function(ev) {
-                    handler(ev);
-                });
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            handler = ko.utils.unwrapObservable(valueAccessor());
+            interceptEvent.bindHandler(element, "swipe", handler);
         },
-        update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {            
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
         }
     };
 
     ko.bindingHandlers.drag = {
-        init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            var handler = ko.utils.unwrapObservable(valueAccessor());
-            $(element).hammer({
-                prevent_default: true
-            })
-                .bind("drag", function(ev) {
-                    handler(ev);
-                });
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            handler = ko.utils.unwrapObservable(valueAccessor());
+            interceptEvent.bindHandler(element, "drag", handler);
         },
-        update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+
         }
     };
 
     ko.bindingHandlers.dragstart = {
         init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            var handler = ko.utils.unwrapObservable(valueAccessor());
-            $(element).hammer({
-                prevent_default: true
-            })
-                .bind("dragstart", function (ev) {
-                    handler(ev);
-                });
+            handler = ko.utils.unwrapObservable(valueAccessor());
+            interceptEvent.bindHandler(element, "dragstart", handler);
         },
         update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
 
@@ -77,13 +138,8 @@
 
     ko.bindingHandlers.dragend = {
         init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            var handler = ko.utils.unwrapObservable(valueAccessor());
-            $(element).hammer({
-                prevent_default: true
-            })
-                .bind("dragend", function (ev) {
-                    handler(ev);
-                });
+            handler = ko.utils.unwrapObservable(valueAccessor());
+            interceptEvent.bindHandler(element, "dragend", handler);
         },
         update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
 
@@ -92,62 +148,78 @@
 
     ko.bindingHandlers.transform = {
         init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            var handler = ko.utils.unwrapObservable(valueAccessor());
-            $(element).hammer({
-                prevent_default: true
-            })
-            .bind("transform", function (ev) {
-                handler(ev);
-            });
+            handler = ko.utils.unwrapObservable(valueAccessor());
+            interceptEvent.bindHandler(element, "transform", handler);
         },
         update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            
+
         }
     };
-    
+
     ko.bindingHandlers.hold = {
         init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            var handler = ko.utils.unwrapObservable(valueAccessor());
-            $(element).hammer({
-                prevent_default: true
-            })
-            .bind("hold", function (ev) {
-                handler(ev);
-            });
+            handler = ko.utils.unwrapObservable(valueAccessor());
+            interceptEvent.bindHandler(element, "hold", handler);
         },
         update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            
+
         }
     };
-    
+
     ko.bindingHandlers.doubletap = {
         init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            var handler = ko.utils.unwrapObservable(valueAccessor());
-            $(element).hammer({
-                prevent_default: true
-            })
-            .bind("doubletap", function (ev) {
-                handler(ev);
-            });
+            handler = ko.utils.unwrapObservable(valueAccessor());
+            interceptEvent.bindHandler(element, "doubletap", handler);
         },
         update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            
+
         }
     };
-    
+
     ko.bindingHandlers.release = {
         init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            var handler = ko.utils.unwrapObservable(valueAccessor());
-            $(element).hammer({
-                prevent_default: true
-            })
-            .bind("release", function (ev) {
-                handler(ev);
-            });
+            handler = ko.utils.unwrapObservable(valueAccessor());
+            interceptEvent.bindHandler(element, "release", handler);
         },
         update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-           
+
         }
     };
-    
-}(window.ko))
+
+}(window.ko);
+
+!function (ko, undefined) {
+    if (typeof (ko) === "undefined") {
+        throw "knockout is required for knockout.triggerTouchEvent";
+    }
+    'use strict';
+
+    ko.utils.triggerTouchEvent = function triggerTouchEvent(element, eventType, canBubble,
+        cancelable, view, detail, screenX, screenY, clientX, clientY, ctrlKey, altKey, shiftKey,
+        metaKey, button, relatedTarget) {
+        //check IE
+        if (document.createEvent && !top.execScript) {
+            var event = document.createEvent('MouseEvents');
+            event.initMouseEvent(eventType, canBubble, cancelable, view, detail, screenX, screenY,
+                clientX, clientY, ctrlKey, altKey, shiftKey, metaKey, button, relatedTarget);
+            element.dispatchEvent(event);
+        }
+        else if (document.createEventObject) {
+            var ev = document.createEventObject();
+            ev.detail = 0;
+            ev.screenX = 12;
+            ev.screenY = 345;
+            ev.clientX = 7;
+            ev.clientY = 220;
+            ev.ctrlKey = false;
+            ev.altKey = false;
+            ev.shiftKey = true;
+            ev.metaKey = false;
+            ev.button = 0;
+            ev.relatedTarget = null;
+            element.fireEvent('on' + eventType, ev);
+        }
+
+    }
+
+}(window.ko);
--- ko.touch/tests/tap.js
+++ ko.touch/tests/tap.js
@@ -1,121 +1,18 @@
-﻿
-test("tap binding test", function () {
-    var div = document.getElementById("tapDiv");
+test('tap binding test', function () {
+    var div = document.getElementById('tapDiv');
     var model = {
         wasCalled: false,
-        doCall: function (arg1) {
-            equal(model.wasCalled, false, "Is false before tap");
+        doCall: function (event) {
+            equal(model.wasCalled, false, 'Is false before tap');
             model.wasCalled = true;
-            equal(model.wasCalled, true, "Is true after tap");
+            equal(model.wasCalled, true, 'Is true after tap');
+            equal(event.testField, 'TestValue', 'event content test value, it signals about our library was involved');
         }
     };
-    
-    div.innerHTML = "<button data-bind='tap:doCall'>tap</button>";
+    div.innerHTML = '<button data-bind="tap:doCall">tap</button>';
     ko.applyBindings(model, div);
-        
-    ko.utils.triggerEvent(div.childNodes[0], "tap");
-    
-    equal(model.wasCalled, true, "We expect tap was called");
-});
+    //Depending on the event, you must pass a different set of arguments
+    ko.utils.triggerTouchEvent(div.firstChild, 'mousedown', true, true, window, 1, 12, 345, 7, 220, false, false, true, false, 0, null);
 
-test("swipe binding test", function () {
-    var div = document.getElementById("swipeDiv");
-    var model = {
-        wasCalled: false,
-        doCall: function (arg1) {
-            equal(model.wasCalled, false, "Is false before swipe");
-            model.wasCalled = true;
-            equal(model.wasCalled, true, "Is true after swipe");
-        }
-    };
-    div.innerHTML = "<button data-bind='swipe:doCall'>swipe</button>";
-    ko.applyBindings(model, div);
-
-    ko.utils.triggerEvent(div.childNodes[0], "swipe");
-    equal(model.wasCalled, true, "We expect swipe was called");
-});
-
-test("drag binding test", function () {
-    var div = document.getElementById("dragDiv");
-    var model = {
-        wasCalled: false,
-        doCall: function (arg1) {
-            equal(model.wasCalled, false, "Is false before drag");
-            model.wasCalled = true;
-            equal(model.wasCalled, true, "Is true after drag");
-        }
-    };
-    div.innerHTML = "<button data-bind='drag:doCall'>drag</button>";
-    ko.applyBindings(model, div);        
-
-    ko.utils.triggerEvent(div.childNodes[0], "drag");
-    equal(model.wasCalled, true, "We expect drag was called");
-});
-
-test("transform binding test", function () {
-    var div = document.getElementById("transformDiv");
-    var model = {
-        wasCalled: false,
-        doCall: function (arg1) {
-            equal(model.wasCalled, false, "Is false before transform");
-            model.wasCalled = true;
-            equal(model.wasCalled, true, "Is true after transform");
-        }
-    };
-    div.innerHTML = "<button data-bind='transform:doCall'>transform</button>";
-    ko.applyBindings(model, div);
-
-    ko.utils.triggerEvent(div.childNodes[0], "transform");
-    equal(model.wasCalled, true, "We expect tap was called");
-});
-
-test("hold binding test", function () {
-    var div = document.getElementById("holdDiv");
-    var model = {
-        wasCalled: false,
-        doCall: function (arg1) {
-            equal(model.wasCalled, false, "Is false before hold");
-            model.wasCalled = true;
-            equal(model.wasCalled, true, "Is true after hold");
-        }
-    };
-    div.innerHTML = "<button data-bind='hold:doCall'>tap</button>";
-    ko.applyBindings(model, div);
-
-    ko.utils.triggerEvent(div.childNodes[0], "hold");
-    equal(model.wasCalled, true, "We expect tap was called");
-});
-
-test("doubletap binding test", function () {
-    var div = document.getElementById("doubletapDiv");
-    var model = {
-        wasCalled: false,
-        doCall: function (arg1) {
-            equal(model.wasCalled, false, "Is false before doubletap");
-            model.wasCalled = true;
-            equal(model.wasCalled, true, "Is true after doubletap");
-        }
-    };
-    div.innerHTML = "<button data-bind='doubletap:doCall'>tap</button>";
-    ko.applyBindings(model, div);
-
-    ko.utils.triggerEvent(div.childNodes[0], "doubletap");
-    equal(model.wasCalled, true, "We expect tap was called");
-});
-
-test("release binding test", function () {
-    var div = document.getElementById("releaseDiv");
-    var model = {
-        wasCalled: false,
-        doCall: function (arg1) {
-            equal(model.wasCalled, false, "Is false before release");
-            model.wasCalled = true;
-            equal(model.wasCalled, true, "Is true after release");
-        }
-    };
-    div.innerHTML = "<button data-bind='release:doCall'>release</button>";
-    ko.applyBindings(model, div);
-
-    ko.utils.triggerEvent(div.childNodes[0], "release");
-    equal(model.wasCalled, true, "We expect tap was called");
+    equal(model.wasCalled, true, 'We expect tap was called');
 });
