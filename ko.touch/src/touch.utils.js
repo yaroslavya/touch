8dd8b9a379337158c71cc6b7b9eaf2526260f555
Yaroslav Yakovlev <yaroslavya@gmail.com>
2013-01-28 18:25:13 +0800

    moved touch trigger to utils

---
+++ ko.touch/src/touch.utils.js
@@ -0,0 +1,28 @@
+﻿ko.utils.triggerTouchEvent = function triggerTouchEvent(element, eventType, canBubble,
+       cancelable, view, detail, screenX, screenY, clientX, clientY, ctrlKey, altKey, shiftKey,
+       metaKey, button, relatedTarget) {
+    //check IE
+    if (document.createEvent && !top.execScript) {
+        var event = document.createEvent('MouseEvents');
+        event.initMouseEvent(eventType, canBubble, cancelable, view, detail, screenX, screenY,
+            clientX, clientY, ctrlKey, altKey, shiftKey, metaKey, button, relatedTarget);
+        element.dispatchEvent(event);
+    }
+    else if (document.createEventObject) {
+        var ev = document.createEventObject();
+        ev.detail = detail;
+        ev.screenX = screenX;
+        ev.screenY = screenY;
+        ev.clientX = clientX;
+        ev.clientY = clientY;
+        ev.ctrlKey = ctrlKey;
+        ev.altKey = altKey;
+        ev.shiftKey = shiftKey;
+        ev.metaKey = metaKey;
+        ev.button = button;
+        ev.relatedTarget = relatedTarget;
+        element.fireEvent('on' + eventType, ev);
+    } else {
+        throw "browser not support trigger events";
+    }
+};
